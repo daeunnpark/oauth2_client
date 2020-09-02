0c52a4766efd461f8a9a7f97c1e497e02ab9f6e8
Daeun Park <daeunn.park@navercorp.com>
2020-09-02 12:39:15 +0900

    reverted to working code

--- src/main/java/com/daeunp/springsecurityclient/config/OauthConfig.java
+++ src/main/java/com/daeunp/springsecurityclient/config/OauthConfig.java
@@ -6,30 +6,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableOAuth2Sso
-public class OauthConfig extends WebSecurityConfigurerAdapter {
+public class OauthConfig extends WebSecurityConfigurerAdapter{
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/", "/login")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.antMatcher("/**")
+			.authorizeRequests()
+			.antMatchers("/", "/login")
+			.permitAll()
+			.anyRequest()
+			.authenticated();
+		
+		/*
+		http.sessionManagement()
+        	.sessionCreationPolicy(SessionCreationPolicy.STATELESS);s
+		 */
+        
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web
+	        .ignoring()
+	        .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	
 }
